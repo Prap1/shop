631267cc5a4e11cde4c9a4304d4dd38dbe3f07d1
Prap1 <81462169+Prap1@users.noreply.github.com>
2023-06-02 21:40:41 +0530

    login-signup page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { LoginPage,SignupPage } from "./Routes.js";
+import {LoginPage,SignupPage} from "./Routes"
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
